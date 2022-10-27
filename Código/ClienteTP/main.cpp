9691cb3a39bcc273f51de3d46e6c579262d6fb16
Daiana eds <daiana_d.s@hotmail.com>
2022-10-26 22:39:50 -0300

    se creo la conexion de servidor y cliente, se agrego menu en cliente seleccionable para enviar al server

---
+++ Código/ClienteTP/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+class Client{
+public:
+    WSADATA WSAData;
+    SOCKET server;
+    SOCKADDR_IN addr;
+    char buffer[1024];
+
+    Client(){
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        server = socket(AF_INET, SOCK_STREAM, 0);
+
+        addr.sin_addr.s_addr = inet_addr("192.168.1.33"); // IP al servidor -> cmd -> ipconfig -> IPv4
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(5555);
+
+        connect(server, (SOCKADDR *)&addr, sizeof(addr));
+        cout << "Conectador al servidor!!" << endl << endl;
+    }
+
+    void EnviarCalculo(){
+
+        cout << "Escribe el mensaje a enviar: " << endl;
+        cin >>this->buffer;
+        send(server,buffer, sizeof(buffer), 0);
+        cout << "Mensaje enviado!!" << endl;
+        memset(buffer, 0, sizeof(buffer)); //reseteamos la variable
+
+    }
+
+    void VerRegistro(){
+
+
+
+    }
+
+    void Recibir(){
+
+        recv(server, buffer, sizeof(buffer), 0);
+        cout << "Servidor dice: " << buffer << endl;
+        memset(buffer, 0, sizeof(buffer)); //resetea el arreglo buffer a 0 (qué variable manipulamos, que se pone, que numero del arreglo(tamaño del buffer se le pone 0))
+
+    }
+
+    void CerrarSesion(){
+
+        closesocket(server);
+        WSACleanup();
+        cout << "Socket cerrado" << endl;
+
+    }
+
+
+    void Menu(){
+        int opcion = 0;
+
+        while(opcion != 3){
+
+            cout<< "SELECCIONE UNA OPCION:\n" <<endl;
+            cout<< "1- Realizar calculo\n" <<endl;
+            cout<< "2- Ver registro de actividades\n" <<endl;
+            cout<< "3- Cerrar sesion\n" <<endl;
+            cin>>opcion;
+            switch(opcion){
+
+                case 1:
+                    system("cls");
+                    cout <<"\nSe usara funcion enviarCalular al servidor" <<endl;
+                    //Cliente->EnviarCalculo();
+                    break;
+
+                case 2:
+                    system("cls");
+                    cout <<"\nSe usara funcion verActividades al servidor" <<endl;
+                    //
+                    break;
+
+                case 3:
+                    system("cls");
+                    cout <<"\nSe usara funcion cerrarSesion al servidor" <<endl;
+                    //Cliente->CerrarSesion();
+                    break;
+
+                default:
+                    system("cls");
+                    cout<<"\nSe ingreso el valor: " << opcion << endl;
+                    cout<<"\nNo es una opcion valida - SELECCIONE UN OPCION DE LAS BRINDADAS\n" <<endl;
+                    break;
+            }
+        }
+    }
+
+
+
+};
+
+int main()
+{
+
+    Client *Cliente = new Client();
+    while(true){
+
+        Cliente->Menu();
+
+    }
+
+}
---
+++ Código/ServidorTP/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+class Server{
+public:
+    WSADATA WSAData;
+    SOCKET server, client;
+    SOCKADDR_IN serverAddr, clientAddr;
+    char buffer[1024];
+
+    Server(){
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        server = socket(AF_INET, SOCK_STREAM, 0);
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(5555);
+
+        bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+        listen(server, 0);
+
+        cout<<"Servidor esperando para conexion. . ." << endl;
+
+        int clientAddrSize = sizeof(clientAddr);
+        if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET){
+            cout << "El cliente se ha conectado!" << endl;
+        }
+    }
+
+
+    void Recibir(){ //para recibir datos del cliente
+
+        recv(client, buffer, sizeof(buffer), 0);
+        cout << "Cliente dice: " << buffer << endl;
+        memset(buffer, 0, sizeof(buffer)); //resetea el arreglo buffer a 0 (qué variable manipulamos, que se pone, que numero del arreglo(tamaño del buffer se le pone 0))
+
+    }
+
+    void Enviar(){ //para enviar al cliente, esto lo voy a usar cuando le tengamos que enviar la respuesta al cliente
+        cout << "Escribe el mensaje a enviar: ";
+        cin>>this->buffer;
+        send(server,buffer, sizeof(buffer), 0);
+        memset(buffer, 0, sizeof(buffer)); //reseteamos la variable
+
+    }
+
+    void CerrarSocket(){ //se cierra el socket
+
+        closesocket(client);
+        cout << "El cliente se ha desconectado. Se ha liberado el socket" << endl;
+
+    }
+};
+
+
+int main()
+{
+
+    Server *Servidor = new Server();
+    while(true){
+
+        Servidor->Recibir();
+        Servidor->Enviar();
+
+    }
+
+}
